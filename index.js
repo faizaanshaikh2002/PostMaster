0d3c70bac506b8a5eb265cc73ebd5d884bd597ba
unknown <faizaanshaikh2002>
2021-09-07 18:25:25 +0530

    final commit

--- index.js
+++ index.js
@@ -1,71 +1,126 @@
-console.log("hi");
+console.log('This is my project 6 from JavaScript course');
 
-// Utility Functions
-// 1. Utility Function to get DOM element from String
-function getElementFromString(string){
-    let div = document.createElement('div')
-    div.innerHTML = string
-    return div.firstElementChild
+// Utility functions:
+// 1. Utility function to get DOM element from string
+function getElementFromString(string) {
+    let div = document.createElement('div');
+    div.innerHTML = string;
+    return div.firstElementChild;
 }
 
 // Initialize no of parameters
-let addedParamscount = 0;
+let addedParamCount = 0;
 
-// Hiding the parameters Box initially
-let parametersBox = document.getElementById("parametersBox")
-parametersBox.style.display = "none"
+// Hide the parameters box initially
+let parametersBox = document.getElementById('parametersBox');
+parametersBox.style.display = 'none';
 
-// Hide the Json Box if parametrs type is selected in content type
-let paramsRadio = document.getElementById("paramsRadio")
+// If the user clicks on params box, hide the json box
+let paramsRadio = document.getElementById('paramsRadio');
 paramsRadio.addEventListener('click', () => {
-    document.getElementById("requestJsonBox").style.display = 'none'
-    document.getElementById("parametersBox").style.display = 'block'
+    document.getElementById('requestJsonBox').style.display = 'none';
+    document.getElementById('parametersBox').style.display = 'block';
 })
 
-// Hide the parameters Box if json type is selected in content type
-let jsonRadio = document.getElementById("jsonRadio")
+// If the user clicks on json box, hide the params box
+let jsonRadio = document.getElementById('jsonRadio');
 jsonRadio.addEventListener('click', () => {
-    document.getElementById("parametersBox").style.display = 'none'
-    document.getElementById("requestJsonBox").style.display = 'block'
+    document.getElementById('requestJsonBox').style.display = 'block';
+    document.getElementById('parametersBox').style.display = 'none';
 })
 
-// If user clicks on add button, add parameter box
-let addParam = document.getElementById("addParam")
+// If the user clicks on + button, add more parameters
+let addParam = document.getElementById('addParam');
 addParam.addEventListener('click', () => {
-    let params = document.getElementById("params");
+    let params = document.getElementById('params');
     let string = `<div class="form-row my-2">
-                    <label for="url" class="col-sm-2 col-form-label">Parameter ${addedParamscount + 2}</label>
+                    <label for="url" class="col-sm-2 col-form-label">Parameter ${addedParamCount + 2}</label>
                     <div class="col-md-4">
-                        <input type="text" class="form-control" id="parameterKey${addedParamscount + 2}" placeholder="Enter Parameter ${addedParamscount + 2} Key">
+                        <input type="text" class="form-control" id="parameterKey${addedParamCount + 2}" placeholder="Enter Parameter ${addedParamCount + 2} Key">
                     </div>
                     <div class="col-md-4">
-                        <input type="text" class="form-control" id="parameterValue${addedParamscount + 2}" placeholder="Enter Parameter ${addedParamscount + 2} Value">
+                        <input type="text" class="form-control" id="parameterValue${addedParamCount + 2}" placeholder="Enter Parameter ${addedParamCount + 2} Value">
                     </div>
                     <button class="btn btn-primary deleteParam"> - </button>
-                </div>`
-    // Converting String to DOM Element Node
-    let paramElement = getElementFromString(string)
-    params.appendChild(paramElement)
-    let deleteParam = document.getElementsByClassName("deleteParam");
-        for(items of deleteParam){
-            items.addEventListener('click', (e)=>{
-                e.target.parentElement.remove()
-            })
+                    </div>`;
+    // Convert the element string to DOM node
+    let paramElement = getElementFromString(string);
+    params.appendChild(paramElement);
+    // Add an event listener to remove the parameter on clicking - button
+    let deleteParam = document.getElementsByClassName('deleteParam');
+    for (item of deleteParam) {
+        item.addEventListener('click', (e) => {
+            // TODO: add a confirmation box to confirm parameter deletion
+            e.target.parentElement.remove();
+        })
+    }
+    addedParamCount++;
+})
+
+// If the user clicks on submit button
+let submit = document.getElementById('submit');
+submit.addEventListener('click', () => {
+    // Show please wait in the response box to request patience from the user
+    // document.getElementById('responseJsonText').value = "Please wait.. Fetching response...";
+    document.getElementById('responsePrism').innerHTML = "Please wait.. Fetching response...";
+
+    // Fetch all the values user has entered
+    let url = document.getElementById("url").value;
+    let requestType = document.querySelector("input[name='requestType']:checked").value;
+    let contentType = document.querySelector("input[name='contentType']:checked").value;
+ 
+    // If user has used params option instead of json, collect all the parameters in an object
+    if (contentType == 'params') {
+        data = {};
+        for (let i = 0; i < addedParamCount + 1; i++) {
+            if (document.getElementById('parameterKey' + (i + 1)) != undefined) {
+                let key = document.getElementById('parameterKey' + (i + 1)).value;
+                let value = document.getElementById('parameterValue' + (i + 1)).value;
+                data[key] = value; 
+            }
         }
-    addedParamscount++;
-})
+        data = JSON.stringify(data);
+    }
+    else {
+        data = document.getElementById('requestJsonText').value;
+    }
 
-// If te users Clicks on Submit button
-let submitBtn = document.getElementById('submit')
-submitBtn.addEventListener('click', ()=>{
-    // Show fetching in response box to 
-    document.getElementById('responseJsonText').value = "Please wait.. Fetching response.."
+    // Log all the values in the console for debugging
+    console.log('URL is ', url);
+    console.log('requestType is ', requestType);
+    console.log('contentType is ', contentType);
+    console.log('data is ', data);
 
-    // Fetch all the values User has entered
-    let url = document.getElementById('url').value;
-    let requestType = document.querySelector("input[name = 'requestType']:checked").value;
-    let contentType = document.querySelector("input[name = 'contentType']:checked").value;
-    console.log(url)
-    console.log(requestType)
-    console.log(contentType)
-})
+    // if the request type is get, invoke fetch api to create a post request
+    if (requestType=='GET'){
+        fetch(url, {
+            method: 'GET',   
+        })
+        .then(response=> response.text())
+        .then((text) =>{
+            // document.getElementById('responseJsonText').value = text;
+            document.getElementById('responsePrism').innerHTML = text;
+            Prism.highlightAll();
+        });
+    }
+
+    else{
+        fetch(url, {
+            method: 'POST', 
+            body: data,
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              }  
+        })
+        .then(response=> response.text())
+        .then((text) =>{
+            // document.getElementById('responseJsonText').value = text;
+            document.getElementById('responsePrism').innerHTML = text;
+            Prism.highlightAll();
+        });
+
+    }
+
+
+});
+
